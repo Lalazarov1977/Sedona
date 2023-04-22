ca24664b9ee71bbd1624543affd140cb7b6af788
Lalazarov1977 <103888250+Lalazarov1977@users.noreply.github.com>
2023-04-22 12:02:55 +0300

    панель навигации для мобайл

---
+++ source/js/script.js
@@ -0,0 +1,32 @@
+(function () {
+    const header = document.querySelector('.page-header');
+    window.onscroll = () => {
+       if (window.pageYOffset > 50) {
+           header.classList.add('header_active');
+       } else {
+           header.classList.remove('header_active');
+       }
+    };
+}());
+   
+// burger handler
+
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.main-nav');
+    const menuClosedItem = document.querySelector ('.main-nav_close');
+    const menuLinks = document.querySelectorAll('.site-list__item');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('main-nav_active');
+    });
+    menuClosedItem.addEventListener('click', () => {
+        menu.classList.remove('main-nav_active');
+    });
+    if (window.innerWidth < 321) {
+        for (let i = 0; i < menuLinks.length; i += 1) {
+            menuLinks[i].addEventListener('click', () => {
+                menu.classList.remove('main_nav_active');
+            });
+        }
+    }    
+}());
